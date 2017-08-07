9f0f48cace25dfe2c18c59c76305a4562518e5d3
prakash kafle <kafleprakash0@gmail.com>
2017-08-07 19:48:01 +0545

    first commit

---
+++ app/helloworld.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core'
+
+
+@Component({
+	selector: 'hello',
+	template:'<strong> HeLLo world.THis is my first app.</strong>'
+
+})
+export class HelloWorldComponent{
+	
+}
